b5b5c1f07c8687a8579071ebb3994fb3edbbf7f4
3TUSK <urey.s.knowledge@gmail.com>
2016-08-30 17:22:17 -0400

    Boring

--- src/main/java/info/tritusk/tabulaplana/TabulaPlana.java
+++ src/main/java/info/tritusk/tabulaplana/TabulaPlana.java
@@ -1,7 +1,5 @@
 package info.tritusk.tabulaplana;
 
-import org.apache.logging.log4j.Logger;
-
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.config.Configuration;
@@ -12,30 +10,23 @@ public class TabulaPlana {
 	@Mod.Instance("tabulaplana")
 	public static TabulaPlana ins;
 	
-	public static Logger log;
-	
 	public static boolean enableStructure;
-	
 	public static String genCode;
-	
-	public static int globalHorizon;
-	public static int globalCloudHeight;
+	public static int heightHorizon, heightCloud;
 	
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		ins = this;
-		log = event.getModLog();
-		
 		final Configuration config = new Configuration(event.getSuggestedConfigurationFile());
 		
 		config.load();
 		
 		enableStructure = config.get("GlobalSettings", "enableStructure", true).getBoolean();
 		genCode = config.get("GlobalSettings", "genCode", "3;minecraft:air;127;decoration").getString();
-		globalHorizon = config.get("GlobalSettings", "globalHorizon", 63).getInt();
-		globalCloudHeight = config.get("GlobalSettings", "globalCloudHeight", 255).getInt();
+		heightHorizon = config.get("GlobalSettings", "globalHorizon", 63).getInt();
+		heightCloud = config.get("GlobalSettings", "globalCloudHeight", 255).getInt();
 		
-		new WorldTypeTabulaPlana(genCode, enableStructure).setCloudHeight(globalCloudHeight).setHorizon(globalHorizon);
+		new WorldTypeTabulaPlana(genCode, enableStructure).setCloudHeight(heightCloud).setHorizon(heightHorizon);
+		event.getModLog().trace("Tabula Plana has successfully loaded.");
 		
 		if (config.hasChanged())
 			config.save();
