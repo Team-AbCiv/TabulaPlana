ae7189e179509abec1c6f5c5fc05f29489c9cc5f
3TUSK <urey.s.knowledge@gmail.com>
2017-04-09 18:54:05 -0400

    Clean up; use SemVer

--- src/main/java/info/tritusk/tabulaplana/TabulaPlana.java
+++ src/main/java/info/tritusk/tabulaplana/TabulaPlana.java
@@ -4,11 +4,15 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.config.Configuration;
 
-@Mod(modid = "tabulaplana", name = "Tabula Plana", version = "R1.0", useMetadata = true)
-public class TabulaPlana {
+@Mod(modid = "tabulaplana", name = "Tabula Plana", version = "@VER@", useMetadata = true)
+public enum TabulaPlana {
 
-	@Mod.Instance("tabulaplana")
-	public static TabulaPlana ins;
+	INSTANCE;
+
+	@Mod.InstanceFactory
+	public static TabulaPlana getInstance() {
+		return INSTANCE;
+	}
 	
 	public static boolean enableStructure;
 	public static String genCode;
@@ -17,17 +21,12 @@ public class TabulaPlana {
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		final Configuration config = new Configuration(event.getSuggestedConfigurationFile());
-		
 		config.load();
-		
 		enableStructure = config.get("GlobalSettings", "enableStructure", true).getBoolean();
 		genCode = config.get("GlobalSettings", "genCode", "3;minecraft:air;127;decoration").getString();
 		heightHorizon = config.get("GlobalSettings", "globalHorizon", 63).getInt();
 		heightCloud = config.get("GlobalSettings", "globalCloudHeight", 255).getInt();
-		
-		new WorldTypeTabulaPlana(genCode, enableStructure).setCloudHeight(heightCloud).setHorizon(heightHorizon);
-		event.getModLog().trace("Tabula Plana has successfully loaded.");
-		
+		new WorldTypeTabulaPlana();
 		if (config.hasChanged())
 			config.save();
 	}
--- src/main/java/info/tritusk/tabulaplana/WorldTypeTabulaPlana.java
+++ src/main/java/info/tritusk/tabulaplana/WorldTypeTabulaPlana.java
@@ -6,42 +6,24 @@ import net.minecraft.world.chunk.IChunkGenerator;
 import net.minecraft.world.gen.ChunkProviderFlat;
 
 public class WorldTypeTabulaPlana extends WorldType {
-	
-	private final String generator;
-	private final boolean structure;
-	private double horizon = 63.0D;
-	private float cloudHeight = 128.0F;
 
-	//Unsafe on generatorCode. Need to workaround
-	public WorldTypeTabulaPlana(String generator, boolean structure) {
+	public WorldTypeTabulaPlana() {
 		super("tabula_plana");
-		this.generator = generator;
-		this.structure = structure;
-	}
-	
-	public WorldTypeTabulaPlana setCloudHeight(float newHeight) {
-		this.cloudHeight = newHeight;
-		return this;
-	}
-	
-	public WorldTypeTabulaPlana setHorizon(double newHorizon) {
-		this.horizon = newHorizon;
-		return this;
 	}
 	
 	@Override
 	public float getCloudHeight() {
-		return this.cloudHeight;
+		return TabulaPlana.heightCloud;
 	}
 
 	@Override
 	public double getHorizon(World world) {
-		return this.horizon;
+		return TabulaPlana.heightHorizon;
 	}
-	
+
 	@Override
 	public IChunkGenerator getChunkGenerator(World world, String generatorOptions) {
-		return new ChunkProviderFlat(world, world.getSeed(), this.structure, this.generator);
+		return new ChunkProviderFlat(world, world.getSeed(), TabulaPlana.enableStructure, TabulaPlana.genCode);
 	}
 
 }
